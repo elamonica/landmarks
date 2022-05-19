5d9ac14308090fef8464ee3a308b9dab05638da1
Ezequiel Lamonica <lamonicaezequiel@gmail.com>
2022-05-19 10:46:01 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ezequiel Lamonica on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
